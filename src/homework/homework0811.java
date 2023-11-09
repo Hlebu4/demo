ac180aa22076ca1de1f8f83edf1b8f804e9addf1
septclean <sept.clean.uk@gmail.com>
2023-11-09 18:26:34 +0100

    commit

---
+++ src/homework/homework0811.java
@@ -0,0 +1,39 @@
+package homework;
+
+public class homework0811{
+    public static void main(String[] args) {
+
+        byte xByte = 127;
+        short xShort = 32767;
+        int xInt = 333333;
+        long xLong = 44444444;
+        float xFloat = 555.5555f;
+        double xDouble = 6666.66666;
+
+        System.out.println("xByte = " + xByte);
+        System.out.println("xShort = " + xShort);
+        System.out.println("xInt = " + xInt);
+        System.out.println("xLong = " + xLong);
+        System.out.println("xFloat = " + xFloat);
+        System.out.println("xDouble = " + xDouble);
+
+        System.out.println("====================");
+
+        System.out.println("xByte = " + xByte + "; xShort = " + xShort + "; xInt = " + xInt + "; xLong = " + xLong + "; xFloat = " + xFloat + "; xDouble = " +xDouble);
+
+        System.out.println("=====================");
+
+        System.out.println("dano: 452");
+
+        int x = 8 / 2;
+        int y = x + 1;
+        int z = y - x + 1;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+
+    }
+
+}
